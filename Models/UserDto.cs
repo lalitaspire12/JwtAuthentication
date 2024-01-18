2ce9449143666b38121e0d8718bece4842addaf9
Lalit Hukam <lalit.hukam@Aspiresys.com>
2024-01-18 10:12:54 +0530

    first

---
+++ Models/UserDto.cs
@@ -0,0 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace LoginJWTApi.Models
+{
+    public class UserDto{
+    public required string Username{get;set;}
+    public required string password{get;set;}
+}
+}
